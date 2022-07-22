17f8f1b5bffd65ce906b1af430f5b7fb3a97684b
soeatsu <soeatsu@gmail.com>
2022-07-22 22:35:58 +0900

    プロジェクト ファイルを追加します。

---
+++ Views/MainWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace ImageJudgment.Views
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
